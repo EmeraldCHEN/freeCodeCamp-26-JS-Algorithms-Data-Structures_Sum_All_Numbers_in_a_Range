852a21c0bcd34f871acaabc1928258443fb569c7
EmeraldNZ <ahyuancy@gmail.com>
2018-12-21 12:20:21 +1300

    Update .js

--- .js
+++ .js
@@ -1,4 +1,7 @@
-function sumAll(arr) {
+
+/********  Solution #1   *******************************************************************************/
+
+function sumAll1(arr) {
     if(arr[0] < arr[1]){
         let sum = arr[1];
         let num = arr[1] - 1;   
@@ -17,11 +20,22 @@ function sumAll(arr) {
         return sum;
     }      
 }
-console.log(sumAll([1, 4])); // 1+2+3+4=10
-console.log(sumAll([4, 1])); // 4+3+2+1=10
+console.log(sumAll1([1, 4])); // 1+2+3+4=10
+console.log(sumAll1([4, 1])); // 4+3+2+1=10
+
+
+/********  Solution #2   *******************************************************************************/
+
+function sumAll2(arr) {
+    let max = Math.max(...arr);
+    let min = Math.min(arr[0], arr[1]);
+    let sum = 0;
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }   
+    return sum;
+}
 
 
 
-
-
-// Retrieved from
+// Retrieved from https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range/
